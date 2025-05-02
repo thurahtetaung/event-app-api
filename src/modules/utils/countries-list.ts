05719dfc185706e7edf9c57fe89bfe98b6f07005
Thura Htet Aung <thurahtetaung711@gmail.com>
2025-05-02 12:44:10 +0700

    updates

--- src/modules/utils/countries-list.ts
+++ src/modules/utils/countries-list.ts
@@ -1,6 +1,6 @@
 // A simplified list of countries. Consider using a library for a full list.
 export const countries = [
-  { code: 'SG', name: 'Singapore' },
+  // { code: 'SG', name: 'Singapore' },
   { code: 'TH', name: 'Thailand' },
   // Add more countries as needed
 ];
